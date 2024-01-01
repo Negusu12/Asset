b057f301773b5c84510452f1d58fd038b5dadf23
Negusu12 <negusu00@yahoo.com>
2024-01-01 10:41:32 -0300

    change db name

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //for MySQLi OOP
-$con = new mysqli('localhost', 'root', '', 'asset');
+$con = new mysqli('localhost', 'root', '', 'asset1');
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
