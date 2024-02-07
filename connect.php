afa1d81e9e82f6a61e3af872c8a91e9d667193a6
Negusu12 <negusu00@yahoo.com>
2024-02-07 10:14:01 -0300

    minor changes

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //for MySQLi OOP
-$con = new mysqli('localhost', 'root', '', 'asset1');
+$con = new mysqli('localhost', 'root', '', 'it_asset');
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
