b98ed48a8cbcfcb2748295022664171d2418d5e3
Negusu12 <negusu00@yahoo.com>
2024-04-19 05:40:43 -0300

    finalized ck text editor

--- footer.php
+++ footer.php
@@ -179,28 +179,6 @@
 <script src="assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- Text Editor 
-<script src="https://cdn.tiny.cloud/1/97skrcesyhw4bznyp5spfe2pnnj075hepezg5hj58dbq2uxi/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
-<script>
-	tinymce.init({
-		selector: 'textarea',
-		plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-		toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-		tinycomments_mode: 'embedded',
-		tinycomments_author: 'Author name',
-		mergetags_list: [{
-				value: 'First.Name',
-				title: 'First Name'
-			},
-			{
-				value: 'Email',
-				title: 'Email'
-			},
-		],
-		ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-	});
-</script>
- End Text Editor -->
 
 <script src="assets/plugins/ckeditor/ckeditor.js"></script>
 <script>
